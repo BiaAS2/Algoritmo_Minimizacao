d1fe27dd5b018c8ee38233daea2386861a11ce34
BiaAS2 <balves.souza14@gmail.com>
2025-09-30 16:55:55 -0300

    first commit

---
+++ src/main.py
@@ -0,0 +1,11 @@
+# Algoritmo de Minimização
+# Grupo:
+# - Beatriz Alves
+# - Renato Noronha
+# - Lucelho Silva
+# - 
+
+# Data: 07/10/2025
+# Disciplina: Análise e Estrutura de Dados
+# Professor: Edyene Oliveira
+
